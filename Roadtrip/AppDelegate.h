a50d98bbd89e2b7c6ce3aea882fed53fae8de38a
Emma Qian <eqian@fb.com>
2018-07-06 10:37:19 -0700

    Initial Commit

---
+++ Roadtrip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Roadtrip
+//
+//  Created by Emma Qian on 7/6/18.
+//  Copyright © 2018 heh17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Roadtrip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Roadtrip
+//
+//  Created by Emma Qian on 7/6/18.
+//  Copyright © 2018 heh17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
