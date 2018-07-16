d38c6261747396ecedff6491fd93fc23b5954abe
Emma Qian <eqian@fb.com>
2018-07-16 14:52:48 -0700

    initialize location view controller

---
+++ Roadtrip/ViewControllers/SelectLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectLocationViewController.h
+//  Roadtrip
+//
+//  Created by Emma Qian on 7/16/18.
+//  Copyright © 2018 heh17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectLocationViewController : UIViewController
+
+@end
