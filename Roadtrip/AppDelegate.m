44d0cbbca1a4a3ad182a2aa1f8e3fc647de6ff5b
Hannah Hsu <hahsu@fb.com>
2018-07-19 15:09:14 -0700

    Changed selectEvents

--- Roadtrip/AppDelegate.m
+++ Roadtrip/AppDelegate.m
@@ -28,7 +28,6 @@
         UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
         
        self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"navController"];
-   
     }
      
     
@@ -36,7 +35,7 @@
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application {
+- (void)applicationWillResignActive:(UIApplication *)application {	
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
